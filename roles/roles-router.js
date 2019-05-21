a0ff7a127977f625d6db8add9cde1fb221c9f059
MAllen07 <mrsmallen07@gmail.com>
2019-05-21 15:37:58 -0400

    made adjustments to the roles-routerjs and the database

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,7 +1,6 @@
 const knex = require("knex");
 const router = require("express").Router();
 
-//use this for each project  - its saying the database using and passing in path to the database
 const config = {
   client: "sqlite3",
   useNullAsDefault: true,
@@ -9,34 +8,54 @@ const config = {
     filename: "./data/rolex.db3"
   }
 };
-//call next - next is a function --valuable is sb to perform quieres 
+
 const db = knex(config);
 
 router.get("/", (req, res) => {
-  //select * from roles
+  //Select * from roles
   db("roles")
     .then(records => res.status(200).json(records))
     .catch(err => res.status(500).json(err));
 });
 
 router.get("/:id", (req, res) => {
-  // retrieve a role by id
-  res.send("Write code to retrieve a role by id");
+  const { id } = req.params;
+  //Select * from roles where id=req.params.id
+  db("roles")
+    .where({ id: id })
+    .first()
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).json(err));
 });
 
 router.post("/", (req, res) => {
-  // add a role to the database
-  res.send("Write code to add a role");
+  const role = req.body;
+  //insert into roles ( name ) values ( 'red.body' )
+  db("roles")
+    .insert(role)
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err));
 });
 
 router.put("/:id", (req, res) => {
-  // update roles
-  res.send("Write code to modify a role");
+  const { id } = req.params;
+  const role = req.body;
+  //update roles set name = req.body.name  where id=req.params.id
+  db("roles")
+    .where({ id })
+    .update(role)
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err));
 });
 
 router.delete("/:id", (req, res) => {
-  // remove roles (inactivate the role)
-  res.send("Write code to remove a role");
+  const { id } = req.params;
+  //delete from roles where id = req.params.id
+  db("roles")
+    .where({ id })
+    .del()
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
